5393a194ad156f298408214792dd0e7787123d73
Philipp Nordhus <philipp@nhus.de>
2012-07-12 16:40:14 +0200

    Initialize uart

--- platform/shimmer/contiki-conf.h
+++ platform/shimmer/contiki-conf.h
@@ -6,6 +6,50 @@
 
 #define WITH_SD				1
 
+/* 1-wire is at p4.7 */
+#define PIN BV(7)
+
+#define PIN_INIT() {\
+  P4DIR &= ~PIN;                /* p4.7 in, resistor pull high */\
+  P4OUT &= ~PIN;                /* p4.7 == 0 but still input */\
+}
+
+/* Set 1-Wire low or high. */
+#define OUTP_0() (P4DIR |=  PIN) /* output and p2.4 == 0 from above */
+#define OUTP_1() (P4DIR &= ~PIN) /* p2.4 in, external resistor pull high */
+
+/* Read one bit. */
+#define INP()    (P4IN & PIN)
+
+/*
+ * Delay for u microseconds on a MSP430 at 8MHz.
+ *
+ * 8 cycles at 8MHz ==> 1us.
+ */
+static void __inline__ brief_pause(register unsigned int n)
+{
+    __asm__ __volatile__ (
+		"1: \n"
+		" dec	%[n] \n"
+		" jne	1b \n"
+        : [n] "+r"(n));
+}
+
+#define udelay(u) brief_pause((u * 4 - 2) / 3)
+
+/*
+ * Where call overhead dominates, use a macro!
+ */
+#define udelay_6() { _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); }
+
+#define UART0_CONF_TX_WITH_INTERRUPT 1
+#define UART0_CONF_RX_WITH_DMA 0
+
+#define UART1_CONF_TX_WITH_INTERRUPT 1
+#define UART1_CONF_RX_WITH_DMA 0
+
+#define SHELL_CONF_PROMPT
+
 //#define NETSTACK_CONF_RADIO		cc1020_driver
 //#define NETSTACK_CONF_RDC		lpp_driver
 //#define NETSTACK_CONF_MAC		csma_driver
@@ -21,36 +65,36 @@
 //#define IRQ_ADC				0x03
 
 /* MSP430 information memory */
-#define INFOMEM_START			0x1000
-#define INFOMEM_BLOCK_SIZE		128
-#define INFOMEM_NODE_ID			0x0000 /* - 0x0004 */
+//#define INFOMEM_START			0x1000
+//#define INFOMEM_BLOCK_SIZE		128
+//#define INFOMEM_NODE_ID			0x0000 /* - 0x0004 */
 
-#define CC_CONF_REGISTER_ARGS		1
-#define CC_CONF_FUNCTION_POINTER_ARGS	1
-#define CC_CONF_INLINE			inline
-#define CC_CONF_VA_ARGS			1
+//#define CC_CONF_REGISTER_ARGS		1
+//#define CC_CONF_FUNCTION_POINTER_ARGS	1
+//#define CC_CONF_INLINE			inline
+//#define CC_CONF_VA_ARGS			1
 
-#define LPP_CONF_LISTEN_TIME		2
-#define LPP_CONF_OFF_TIME		(CLOCK_SECOND - (LPP_CONF_LISTEN_TIME))
-#define QUEUEBUF_CONF_NUM		4
+//#define LPP_CONF_LISTEN_TIME		2
+//#define LPP_CONF_OFF_TIME		(CLOCK_SECOND - (LPP_CONF_LISTEN_TIME))
+//#define QUEUEBUF_CONF_NUM		4
 
 
 #define CCIF
 #define CLIF
 
 /* Clear channel assessment timeout for sending with the CC1020 radio. (ms) */
-#define CC1020_CONF_CCA_TIMEOUT		10
+//#define CC1020_CONF_CCA_TIMEOUT		10
 
 /* Clock */
 typedef unsigned clock_time_t;
 #define CLOCK_CONF_SECOND		64
-#define F_CPU				2457600uL /* CPU target speed in Hz. */
+#define F_CPU				8000000uL /* CPU target speed in Hz. */
 
-#define BAUD2UBR(baud)			(F_CPU/(baud))
+//#define BAUD2UBR(baud)			(F_CPU/(baud))
 
-#include "ctk/ctk-vncarch.h"
+//#include "ctk/ctk-vncarch.h"
 
-#define LOG_CONF_ENABLED		0
+//#define LOG_CONF_ENABLED		0
 
 /**
  * The statistics data type.
@@ -58,8 +102,10 @@ typedef unsigned clock_time_t;
  * This datatype determines how high the statistics counters are able
  * to count.
  */
-typedef uint16_t uip_stats_t;
 
+
+typedef uint16_t uip_stats_t;
+/*
 typedef int bool;
 #define	TRUE				1
 #define FALSE 				0
@@ -83,7 +129,7 @@ typedef int bool;
 
 #define ELFLOADER_CONF_DATAMEMORY_SIZE	100
 #define ELFLOADER_CONF_TEXTMEMORY_SIZE	0x1000
-
+*/
 /* LEDs ports MSB430 */
 #define LEDS_PxDIR P4DIR
 #define LEDS_PxOUT P4OUT
---
+++ platform/shimmer/contiki-shimmer-main.c
@@ -0,0 +1,163 @@
+#include "contiki.h"
+#include "dev/leds.h"
+#include "dev/uart0.h"
+#include "dev/ds2411.h"
+#include "dev/serial-line.h"
+#include "dev/watchdog.h"
+#include "isr_compat.h"
+
+//SENSORS(NULL);
+
+int putchar(int c)
+{
+    uart0_writeb(c);
+    return c;
+}
+
+int dock_connected(void)
+{
+	if (P2IN & 0x08) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+static void
+msb_ports_init(void)
+{
+  P1SEL = 0xC0; P1OUT = 0x00; P1DIR = 0x80;
+  P2SEL = 0x00; P2OUT = 0x00; P2DIR = 0x00;
+  P3SEL = 0x00; P3OUT = 0x00; P3DIR = 0x00;
+  P4SEL = 0x00; P4OUT = 0x00; P4DIR = 0x00;
+  P5SEL = 0x00; P5OUT = 0x00; P5DIR = 0x00;
+  P6SEL = 0x00; P6OUT = 0x00; P6DIR = 0x00;
+}
+
+static int uart0_input(unsigned char c)
+{
+    serial_line_input_byte(c);
+    return 0;
+}
+
+static int uart1_input(unsigned char c)
+{
+    leds_on(LEDS_YELLOW);
+    serial_line_input_byte(c);
+    return 0;
+}
+
+int
+main(void)
+{
+  msp430_cpu_init();
+  watchdog_stop();
+
+  /* Platform-specific initialization. */
+  msb_ports_init();
+//  adc_init();
+
+  clock_init();
+  rtimer_init();
+
+// use XT2 as main clock, set clock divder for SMCLK to 8
+// MLCK:   8 MHz
+// SMCLK:  8 MHz
+// ACLK:  32.768 kHz
+  BCSCTL1 = RSEL2 | RSEL1 | RSEL0;
+  BCSCTL2 = SELM1 | SELS;// | DIVS0 | DIVS1;
+
+//  sht11_init();
+  leds_init();
+  leds_on(LEDS_ALL);
+
+  uart0_init(0x45);
+  uart0_set_input(uart0_input);
+
+  uart1_init(0x45);
+  uart1_set_input(uart1_input);
+
+  process_init();
+
+  /* System timers */
+  process_start(&etimer_process, NULL);
+  ctimer_init();
+
+  serial_line_init();
+
+  leds_off(LEDS_ALL);
+
+  ds2411_init();
+
+  // University of California Berkeley's OUI
+  ds2411_id[0] = 0x00;
+  ds2411_id[1] = 0x12;
+  ds2411_id[2] = 0x6d;
+
+  // Following two octets must be 'LO' -- "local" in order to use UCB's OUI
+  ds2411_id[3] = 'L';
+  ds2411_id[4] = 'O';
+
+  autostart_start(autostart_processes);
+
+  /*
+   * This is the scheduler loop.
+   */
+  ENERGEST_ON(ENERGEST_TYPE_CPU);
+
+  while (1) {
+    int r;
+#if PROFILE_CONF_ON
+    profile_episode_start();
+#endif /* PROFILE_CONF_ON */
+    do {
+      /* Reset watchdog. */
+      watchdog_periodic();
+      r = process_run();
+    } while(r > 0);
+
+#if PROFILE_CONF_ON
+    profile_episode_end();
+#endif /* PROFILE_CONF_ON */
+
+    /*
+     * Idle processing.
+     */
+    int s = splhigh();		/* Disable interrupts. */
+    if (process_nevents() != 0) {
+      splx(s);			/* Re-enable interrupts. */
+    } else {
+      static unsigned long irq_energest = 0;
+      /* Re-enable interrupts and go to sleep atomically. */
+      ENERGEST_OFF(ENERGEST_TYPE_CPU);
+      ENERGEST_ON(ENERGEST_TYPE_LPM);
+     /*
+      * We only want to measure the processing done in IRQs when we
+      * are asleep, so we discard the processing time done when we
+      * were awake.
+      */
+      energest_type_set(ENERGEST_TYPE_IRQ, irq_energest);
+/*
+      if (uart_edge) {
+	_BIC_SR(LPM1_bits + GIE);
+      } else {
+	_BIS_SR(LPM1_bits + GIE);
+      }
+*/
+      /*
+       * We get the current processing time for interrupts that was
+       * done during the LPM and store it for next time around. 
+       */
+      dint();
+      irq_energest = energest_type_time(ENERGEST_TYPE_IRQ);
+      eint();
+      ENERGEST_OFF(ENERGEST_TYPE_LPM);
+      ENERGEST_ON(ENERGEST_TYPE_CPU);
+#if PROFILE_CONF_ON
+      profile_clear_timestamps();
+#endif /* PROFILE_CONF_ON */
+    }
+  }
+
+  return 0;
+}
