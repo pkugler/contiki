2f9cb1bce051b4c9d88ac758db9ebb85531f064f
Philipp Nordhus <philipp@nhus.de>
2012-08-01 19:00:28 +0200

    Add function to start sampling

--- platform/shimmer/dev/hmc5843.c
+++ platform/shimmer/dev/hmc5843.c
@@ -4,8 +4,32 @@
 
 #define HMC5843_ID (0x1e)
 HWCONF_PIN(GYRO_PWREN_N, 1, 1)
+static uint8_t command[2];
 
-static uint8_t reg;
+enum hmc5843_register
+{
+    HMC5843_CONFIG_A    =  0,
+    HMC5843_CONFIG_B    =  1,
+    HMC5843_MODE        =  2,
+    HMC5843_OUTPUT_X_H  =  3,
+    HMC5843_OUTPUT_X_L  =  4,
+    HMC5843_OUTPUT_Y_H  =  5,
+    HMC5843_OUTPUT_Y_L  =  6,
+    HMC5843_OUTPUT_Z_H  =  7,
+    HMC5843_OUTPUT_Z_L  =  8,
+    HMC5843_STATUS      =  9,
+    HMC5843_ID_A        = 10,
+    HMC5843_ID_B        = 11,
+    HMC5843_ID_C        = 12
+};
+
+enum hmc5843_mode
+{
+    HMC5843_MODE_CONTINUOUS = 0x00,
+    HMC5843_MODE_SINGLE     = 0x01,
+    HMC5843_MODE_IDLE       = 0x02,
+    HMC5843_MODE_SLEEP      = 0x03
+};
 
 void hmc5843_enable(void)
 {
@@ -22,10 +46,17 @@ void hmc5843_disable(void)
     udelay(5000);
 }
 
+void hmc5843_start_continuous(void)
+{
+    command[0] = HMC5843_MODE;
+    command[1] = HMC5843_MODE_CONTINUOUS;
+    uart0_i2c_write(HMC5843_ID, command, 2, NULL);
+}
+
 void hmc5843_prepare_read(void)
 {
-    reg = 0x03; // first data register
-    uart0_i2c_write(HMC5843_ID, &reg, 1, NULL);
+    command[0] = HMC5843_OUTPUT_X_H;
+    uart0_i2c_write(HMC5843_ID, command, 1, NULL);
 }
 
 void hmc5843_read_data(uint8_t *buffer, uint8_t size, uart0_i2c_callback callback)
--- platform/shimmer/dev/hmc5843.h
+++ platform/shimmer/dev/hmc5843.h
@@ -5,6 +5,7 @@
 
 void hmc5843_enable(void);
 void hmc5843_disable(void);
+void hmc5843_start_continuous(void);
 void hmc5843_read_data(uint8_t *buffer, uint8_t size, uart0_i2c_callback callback);
 
 #endif // HMC5843_H
